731f052e14a7a0822fe63baca8cf407a31002067
Clément <clement.guibout@gmail.com>
2021-07-21 22:56:15 +0200

    Added print button and information checks before printing

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="card">
-    <vue-qrcode :value="value" errorCorrectionLevel="H" />
+    <div @input="updateNetworkEncryptionType($event.target.value)">
+      <input type="radio" id="WEP" name="networkEncryptionType" value="WEP">
+      <label for="WEP">WEP</label>
 
-    <div @input="renderQRCode()">
-      <div @input="updateNetworkEncryptionType($event.target.value)">
-        <input type="radio" id="WEP" name="networkEncryptionType" value="WEP">
-        <label for="WEP">WEP</label>
+      <input type="radio" id="WPA/WPA2" name="networkEncryptionType" value="WPA" checked>
+      <label for="WPA/WPA2">WPA/WPA2</label>
 
-        <input type="radio" id="WPA/WPA2" name="networkEncryptionType" value="WPA" checked>
-        <label for="WPA/WPA2">WPA/WPA2</label>
+      <input type="radio" id="None" name="networkEncryptionType" value="nopass">
+      <label for="None">None</label>
+    </div>
 
-        <input type="radio" id="None" name="networkEncryptionType" value="nopass">
-        <label for="None">None</label>
-      </div>
+    <div id="printable" @input="renderQRCode()">
+      <vue-qrcode :value="value" errorCorrectionLevel="H" />
 
       <label for="wifi-ssid">Wifi name</label>
       <input name="wifi-ssid" id="wifi-ssid" @input="updateQRCodeSSID($event.target.value)" required>
@@ -20,6 +20,8 @@
       <label for="wifi-password">Wifi password</label>
       <input name="wifi-password" id="wifi-password" min="8" @input="updateQRCodePassword($event.target.value)">
     </div>
+
+    <button type="button" v-on:click="print()">Print</button>
   </div>
 </template>
 
@@ -55,6 +57,25 @@ export default {
           ";S:" + this.ssid +
           ";P:" + this.password +
           ";;";
+    },
+    print() {
+      if (! (this.ssid.length > 0)) return alert("Wifi name cannot be empty!");
+      if (this.network_encryption === "WPA" && ! (this.password.length >= 8))
+        return alert("Password must be at least 8 characters long!");
+      if (this.network_encryption === "WEP" && ! (this.password.length >= 6))
+        return alert("Password must be at least 6 characters long!");
+
+      let page = window.open("", "PRINT");
+
+      page.document.write("<!DOCTYPE html><html lang='en'><head><title>" + document.title + "</title>");
+      page.document.write("</head><body>");
+      page.document.write(document.getElementById("printable").innerHTML);
+      page.document.write("</body></html>");
+
+      page.document.close();
+      page.focus();
+      page.print();
+      page.close();
     }
   }
 }
